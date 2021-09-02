00039a15eda8836d34b48b64ffb8c0587f699925
anikdhar11 <aniksutradhar11@gmail.com>
2021-09-02 19:03:01 +0600

    done

--- js/app.js
+++ js/app.js
@@ -1,37 +1,53 @@
+const total = document.getElementById('total-result');
+const errorDiv = document.getElementById('error');
+
 const loadData = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    searchField.value = "";
-    document.getElementById('displaybook').textContent = "";
-   const url =`https://openlibrary.org/search.json?q=${searchText}`;
-   fetch(url)
-   .then(res => res.json())
-   .then(data => displayShowBook(data.docs))
-  
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  // searchfield clear
+  searchField.value = "";
+  total.style.display = 'none';
+  document.getElementById('displaybook').textContent = "";
+  errorDiv.innerText = '';
+  //handle empty input
+  if (searchText == "") {
+    errorDiv.innerText = 'Search field can not be empty';
+  }
+  else {
+     const url = `https://openlibrary.org/search.json?q=${searchText}`;
+       fetch(url)
+      .then(res => res.json())
+      .then(data => displayShowBook(data.docs))
+  }
 }
 
 const displayShowBook = books => {
-    const displayBook = document.getElementById('displaybook');
-     books.forEach( book =>{
-     console.log(book);
-     const div = document.createElement('div');
-     div.classList.add('col','w-50');
-     div.innerHTML = `
-     <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid" alt="...">
+  const displayBook = document.getElementById('displaybook');
+  books.forEach(book => {
+    //  console.log(book);
+    total.style.display = 'block';
+    errorDiv.innerText = '';
+    total.innerText = `Total result:${books.length}`;
+    const div = document.createElement('div');
+    div.classList.add('col', 'w-50', 'shadow-lg');
+    div.innerHTML = `
+     <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
      <div class="card mb-3 text-center">
      <h1 class="card-header bg-dark
-     border-light text-light">Book Name: ${book.title}</h1>
+      text-light">${book.title}</h1>
      <div class="card-body text-success bg-dark">
-       <h2 class="card-title">Book Author: ${book.author_name[0]}</h2>
-       <h4 class="card-text">First Publish: ${book.first_publish_year}</h4>
+          <h2 class="card-title">${book.author_name[0] ? book.author_name[0] : book.author_name}</h2>
+          <h4 class="card-text">First Publish: ${book.first_publish_year ? book.first_publish_year : 'not avaiable'}</h4>
      </div>
      <h4 class="card-footer bg-dark text-secondary">Publisher: ${book.publisher[0]}</h4>
    </div>`
-   displayBook.appendChild(div);
+    displayBook.appendChild(div);
 
- })
+  })
 }
 
-   
+
+
+
 
 
